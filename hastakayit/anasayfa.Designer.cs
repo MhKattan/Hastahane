4a17b5feb27dfd18af01b42f07561238cc9edac8
HP <HP@DESKTOP-41M6AUQ>
2023-08-23 22:52:45 +0300

    Proje dosyası ekle.

---
+++ hastakayit/anasayfa.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace hastakayit
+{
+    partial class anasayfa
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.adminToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.personelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hastaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tanımlarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.meslekTanımıToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hastaKonumToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.çalışmaTablosuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hastalıkTablosuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.şehirTanımıToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.medeniDurumToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.öğrenimDurumToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sağlıkTanımıToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ziyaretTablosuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.personelKedemiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.raporlarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.çıkışToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.adminToolStripMenuItem,
+            this.personelToolStripMenuItem,
+            this.hastaToolStripMenuItem,
+            this.tanımlarToolStripMenuItem,
+            this.raporlarToolStripMenuItem,
+            this.çıkışToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(860, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // adminToolStripMenuItem
+            // 
+            this.adminToolStripMenuItem.Name = "adminToolStripMenuItem";
+            this.adminToolStripMenuItem.Size = new System.Drawing.Size(67, 24);
+            this.adminToolStripMenuItem.Text = "Admin";
+            // 
+            // personelToolStripMenuItem
+            // 
+            this.personelToolStripMenuItem.Name = "personelToolStripMenuItem";
+            this.personelToolStripMenuItem.Size = new System.Drawing.Size(78, 26);
+            this.personelToolStripMenuItem.Text = "Personel";
+            // 
+            // hastaToolStripMenuItem
+            // 
+            this.hastaToolStripMenuItem.Name = "hastaToolStripMenuItem";
+            this.hastaToolStripMenuItem.Size = new System.Drawing.Size(61, 24);
+            this.hastaToolStripMenuItem.Text = "Hasta";
+            // 
+            // tanımlarToolStripMenuItem
+            // 
+            this.tanımlarToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.meslekTanımıToolStripMenuItem,
+            this.hastaKonumToolStripMenuItem,
+            this.çalışmaTablosuToolStripMenuItem,
+            this.hastalıkTablosuToolStripMenuItem,
+            this.şehirTanımıToolStripMenuItem,
+            this.medeniDurumToolStripMenuItem,
+            this.öğrenimDurumToolStripMenuItem,
+            this.sağlıkTanımıToolStripMenuItem,
+            this.ziyaretTablosuToolStripMenuItem,
+            this.personelKedemiToolStripMenuItem});
+            this.tanımlarToolStripMenuItem.Name = "tanımlarToolStripMenuItem";
+            this.tanımlarToolStripMenuItem.Size = new System.Drawing.Size(79, 26);
+            this.tanımlarToolStripMenuItem.Text = "Tanımlar";
+            // 
+            // meslekTanımıToolStripMenuItem
+            // 
+            this.meslekTanımıToolStripMenuItem.Name = "meslekTanımıToolStripMenuItem";
+            this.meslekTanımıToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.meslekTanımıToolStripMenuItem.Text = "Meslek Tanımı";
+            // 
+            // hastaKonumToolStripMenuItem
+            // 
+            this.hastaKonumToolStripMenuItem.Name = "hastaKonumToolStripMenuItem";
+            this.hastaKonumToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.hastaKonumToolStripMenuItem.Text = "Hasta Konum";
+            // 
+            // çalışmaTablosuToolStripMenuItem
+            // 
+            this.çalışmaTablosuToolStripMenuItem.Name = "çalışmaTablosuToolStripMenuItem";
+            this.çalışmaTablosuToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.çalışmaTablosuToolStripMenuItem.Text = "Çalışma Tablosu";
+            // 
+            // hastalıkTablosuToolStripMenuItem
+            // 
+            this.hastalıkTablosuToolStripMenuItem.Name = "hastalıkTablosuToolStripMenuItem";
+            this.hastalıkTablosuToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.hastalıkTablosuToolStripMenuItem.Text = "Hastalık Tablosu";
+            // 
+            // şehirTanımıToolStripMenuItem
+            // 
+            this.şehirTanımıToolStripMenuItem.Name = "şehirTanımıToolStripMenuItem";
+            this.şehirTanımıToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.şehirTanımıToolStripMenuItem.Text = "Şehir Tanımı";
+            // 
+            // medeniDurumToolStripMenuItem
+            // 
+            this.medeniDurumToolStripMenuItem.Name = "medeniDurumToolStripMenuItem";
+            this.medeniDurumToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.medeniDurumToolStripMenuItem.Text = "Medeni Durum";
+            // 
+            // öğrenimDurumToolStripMenuItem
+            // 
+            this.öğrenimDurumToolStripMenuItem.Name = "öğrenimDurumToolStripMenuItem";
+            this.öğrenimDurumToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.öğrenimDurumToolStripMenuItem.Text = "Öğrenim Durum";
+            // 
+            // sağlıkTanımıToolStripMenuItem
+            // 
+            this.sağlıkTanımıToolStripMenuItem.Name = "sağlıkTanımıToolStripMenuItem";
+            this.sağlıkTanımıToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.sağlıkTanımıToolStripMenuItem.Text = "Sağlık Tanımı";
+            // 
+            // ziyaretTablosuToolStripMenuItem
+            // 
+            this.ziyaretTablosuToolStripMenuItem.Name = "ziyaretTablosuToolStripMenuItem";
+            this.ziyaretTablosuToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.ziyaretTablosuToolStripMenuItem.Text = "Ziyaret Tanımı";
+            // 
+            // personelKedemiToolStripMenuItem
+            // 
+            this.personelKedemiToolStripMenuItem.Name = "personelKedemiToolStripMenuItem";
+            this.personelKedemiToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.personelKedemiToolStripMenuItem.Text = "Personel Kedemi";
+            // 
+            // raporlarToolStripMenuItem
+            // 
+            this.raporlarToolStripMenuItem.Name = "raporlarToolStripMenuItem";
+            this.raporlarToolStripMenuItem.Size = new System.Drawing.Size(80, 26);
+            this.raporlarToolStripMenuItem.Text = "Raporlar";
+            // 
+            // çıkışToolStripMenuItem
+            // 
+            this.çıkışToolStripMenuItem.Name = "çıkışToolStripMenuItem";
+            this.çıkışToolStripMenuItem.Size = new System.Drawing.Size(53, 26);
+            this.çıkışToolStripMenuItem.Text = "Çıkış";
+            // 
+            // anasayfa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(860, 460);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "anasayfa";
+            this.Text = "anasayfa";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem adminToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem personelToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hastaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tanımlarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem meslekTanımıToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hastaKonumToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem çalışmaTablosuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hastalıkTablosuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem şehirTanımıToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem medeniDurumToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem öğrenimDurumToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sağlıkTanımıToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ziyaretTablosuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem personelKedemiToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem raporlarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem çıkışToolStripMenuItem;
+    }
+}
