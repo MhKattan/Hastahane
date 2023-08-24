4a494ab933752bc029944ecfd4a2fde498c93891
Muhammet Kattan <muh.kattan@gmail.com>
2023-08-25 01:44:24 +0300

    hastalik işlendi

---
+++ hastakayit/hastalik.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace hastakayit
+{
+    partial class hastalik
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.hastakik_adi = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hastalık Adı";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(154, 86);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(150, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(154, 46);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(114, 22);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(390, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 34);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Ekle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(390, 80);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 34);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Düzeltme";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(481, 40);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(85, 34);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Silme";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(481, 80);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(85, 34);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Arama";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Id,
+            this.hastakik_adi});
+            this.dataGridView1.Location = new System.Drawing.Point(49, 208);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(624, 187);
+            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.Click += new System.EventHandler(this.dataGridView1_Click);
+            // 
+            // Id
+            // 
+            this.Id.DataPropertyName = "Id";
+            this.Id.HeaderText = "Sıra No";
+            this.Id.MinimumWidth = 6;
+            this.Id.Name = "Id";
+            this.Id.Width = 125;
+            // 
+            // hastakik_adi
+            // 
+            this.hastakik_adi.DataPropertyName = "hastalik_adi";
+            this.hastakik_adi.HeaderText = "Hastalık Adı";
+            this.hastakik_adi.MinimumWidth = 6;
+            this.hastakik_adi.Name = "hastakik_adi";
+            this.hastakik_adi.Width = 125;
+            // 
+            // hastalik
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "hastalik";
+            this.Text = "hastalik";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.hastalik_FormClosing);
+            this.Load += new System.EventHandler(this.hastalik_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn hastakik_adi;
+    }
+}
---
+++ hastakayit/hastalik.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using Microsoft.VisualBasic;
+
+namespace hastakayit
+{
+    public partial class hastalik : Form
+    {
+        public hastalik()
+        {
+            InitializeComponent();
+        }
+
+        kutuphane veri=new kutuphane();
+        private void hastalik_Load(object sender, EventArgs e)
+        {
+            veri.openconnection();
+            doldur();
+        }
+
+        private void hastalik_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            veri.closeconnection();
+        }
+        private void doldur()
+        {
+            dataGridView1.DataSource = veri.doldur("select *from hastalik");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string cumle="insert into hastalik (hastalik_adi)values('"+textBox1.Text+"')";
+            veri.crud(cumle);
+            doldur();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                MessageBox.Show("Lütfen Listeden Seçim Yapınız");
+            }
+            else
+            {
+                string cumle = "update hastalik set hastalik_adi='" + textBox1.Text + "' where Id='" + textBox2.Text + "'";
+                veri.crud(cumle);
+                doldur();
+            }
+            
+        }
+
+        private void dataGridView1_Click(object sender, EventArgs e)
+        {
+            int secim = dataGridView1.CurrentCell.RowIndex;
+            textBox1.Text = dataGridView1.Rows[secim].Cells[1].Value.ToString();
+            textBox2.Text = dataGridView1.Rows[secim].Cells[0].Value.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DialogResult cevap = MessageBox.Show("Bu Kaydı Silmek İstiyor musunuz", "Dikkat", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (cevap == DialogResult.Yes)
+            {
+                string cumle = "delete from hastalik where Id='" + textBox2.Text + "'";
+                veri.crud(cumle);
+                doldur();
+            }
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string ara = Interaction.InputBox("Hastalık Adını Girin");
+            string cumle = "Select * from hastalik where hastalik_adi='" + ara + "'";
+            dataGridView1.DataSource = veri.doldur(cumle);
+        }
+    }
+}
